9d135a917fd7817cd4432d3e4ab0ce262a8245f0
Larry-PickUp <larry.contact@gmail.com>
2017-10-12 14:46:01 +0200

    rajout page static

---
+++ demo__simple-node-container/index-simple.js
@@ -0,0 +1,44 @@
+const hapi = require("hapi");
+const Inert = require("inert");
+
+const server = new hapi.Server();
+
+server.connection(
+    {
+        port: 3030
+    }
+);
+
+// Register plugin's ===
+server.register(Inert, (err) => {
+    if (err) {
+        throw err;
+    }
+});
+
+// index route
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: './public/',
+            listing: false,
+            index: true
+        }
+    },
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+    }
+});
+
+//=== GO ====
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("The Magic Happens at:", server.info.uri);
+});
